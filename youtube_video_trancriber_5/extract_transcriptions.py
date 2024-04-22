86ab0e8ef86e1731d902ece16cde82041ac6902c
pakshaljain05 <56919361+pakshaljain05@users.noreply.github.com>
2024-04-22 23:37:49 +0530

    modules added

--- youtube_video_trancriber_5/extract_transcriptions.py
+++ youtube_video_trancriber_5/extract_transcriptions.py
@@ -13,7 +13,7 @@ import scrapetube
 import time
 
 channel_name = input()                        # user input 
-youtube_api_key = os.getenv("YOUTUBE_API_KEY")
+# youtube_api_key = os.getenv("YOUTUBE_API_KEY")
 
 # def get_channel_id(channel_name,youtube_api_key):
 #     try:
@@ -21,46 +21,51 @@ youtube_api_key = os.getenv("YOUTUBE_API_KEY")
 #     except Exception as e:
 #         print(e)
 #     return channel_id
+def scrape_videos(channel_name, sort_by, limit):
+    error_video_lists=[]
+    x=[]
+    y=['video_id','video_title','publish_time','total_views']
+    videos = scrapetube.get_channel(  
+        # channel_id = channel_id,
+        # channel_url: str = None,
+        channel_username = channel_name,
+        limit = limit,             # set limit 10 to competitors videoids change the filter too 
+        sleep = 1,
+        # proxies: dict = None,
+        sort_by = sort_by
+        # sort_by = "newest",                  #     Literal["newest", "oldest", "popular"]
+        content_type = "videos"   #    Literal["videos", "shorts", "streams"]
+        )
 
-error_video_lists=[]
-x=[]
-y=['video_id','video_title','publish_time','total_views']
-videos = scrapetube.get_channel(  
-    # channel_id = channel_id,
-    # channel_url: str = None,
-    channel_username = channel_name,
-    # limit = 1,             # set limit 10 to competitors videoids change the filter too 
-    sleep = 1,
-    # proxies: dict = None,
-    sort_by = "newest",                  #     Literal["newest", "oldest", "popular"]
-    content_type = "videos"   #    Literal["videos", "shorts", "streams"]
-    )
+    for video in videos:
+    
+    
+        video_id=video['videoId']
+        video_title=video['title']['runs'][0]['text']
+        # print(video_title)
+        publish_time=video['publishedTimeText']['simpleText']
+        try:
+            total_views=video['viewCountText']['simpleText']
+        except:
+            print(video_title)
+            error_video_lists.append(video_title)
 
-for video in videos:
-   
-   
-    video_id=video['videoId']
-    video_title=video['title']['runs'][0]['text']
-    # print(video_title)
-    publish_time=video['publishedTimeText']['simpleText']
-    try:
-        total_views=video['viewCountText']['simpleText']
-    except:
-        print(video_title)
-        error_video_lists.append(video_title)
+            total_views = 'PREMIUM'
+        x.append([video_id,video_title,publish_time,total_views])
 
-        total_views = 'PREMIUM'
-    x.append([video_id,video_title,publish_time,total_views])
-
-video_df=pd.DataFrame(data=x,columns=y)
+    video_df=pd.DataFrame(data=x,columns=y)
+    return video_df
 
 def view_split(x):
     x=int(x.split(' ')[0].replace(',',''))
     return x
 
-video_df=video_df.drop_duplicates(['video_id'],keep='first').reset_index(drop=True)
-video_df['view_count']=video_df['total_views'].apply(view_split)
-video_ids=video_df['video_id'].to_list()
+def video_preprocessing(video_df):
+    video_df=video_df.drop_duplicates(['video_id'],keep='first').reset_index(drop=True)
+    video_df['view_count']=video_df['total_views'].apply(view_split)
+    video_ids=video_df['video_id'].to_list()
+    return video_ids
+
 def extract_transcript_details(video_ids):
     transcript_list=[]
     errors_list=[]
@@ -85,6 +90,6 @@ def extract_transcript_details(video_ids):
             # pass
         # raise e
 
-transcripts_list = extract_transcript_details(video_ids)
-video_df['transcript'] = transcripts_list
-video_df.to_csv(f'data\\transcripts_data\{channel_name}_transcripts.csv')
+# transcripts_list = extract_transcript_details(video_ids)
+# video_df['transcript'] = transcripts_list
+# video_df.to_csv(f'data\\transcripts_data\{channel_name}_transcripts.csv')
